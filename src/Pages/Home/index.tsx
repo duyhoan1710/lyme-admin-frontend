333cfba9258c4e3ce2694a35b112d497f9cca0ad
nguyen duy hoan <hoanduy1710@gmail.com>
2022-09-08 17:55:12 +0700

    add: code

--- src/Pages/Home/index.tsx
+++ src/Pages/Home/index.tsx
@@ -1,12 +1,20 @@
 import React, { useEffect, useState } from "react";
-import { INTRO } from "src/Constants";
 import styled from "styled-components";
 import { socket } from "src/Utils/socket";
+import { Button, Image, Input } from "antd";
 
 const newSocket = socket();
 
+interface IUser {
+    id: string;
+    name: string;
+}
+
 export const Home = () => {
     const [sk] = useState(newSocket.init());
+    const [username, setUsername] = useState<IUser>();
+    const [listUser, setListUser] = useState<IUser[]>([]);
+
     const onClick = () => {
         sk.emit("send-message", "test");
     };
@@ -24,15 +32,36 @@ export const Home = () => {
 
     return (
         <StyledHome>
-            <p className="intro" onClick={onClick}>
-                {INTRO}
-            </p>
+            <div className="left-block">
+                <div className="form-name">
+                    <Input
+                        className="input-name"
+                        placeholder="username"
+                        // onChange={(e) => setUsername(e.target.value)}
+                    />
+                    <Button className="btn-submit">submit</Button>
+                </div>
+
+                <div className="list-user">
+                    <div className="title">List User</div>
+                    {listUser?.length
+                        ? listUser?.map((user: IUser) => (
+                              <div key={user.id} className="user">
+                                  <Image className="image-user" />
+                                  <div>{user.name}</div>
+                              </div>
+                          ))
+                        : ""}
+                </div>
+            </div>
+
+            <div className="right-block"></div>
         </StyledHome>
     );
 };
 
 const StyledHome = styled.div`
-    height: 100vh;
+    height: 100%;
     width: 100%;
     display: flex;
     align-items: center;
@@ -40,5 +69,41 @@ const StyledHome = styled.div`
     .intro {
         font-size: 25px;
         color: #2a2550;
+    }
+
+    .left-block {
+        width: 25%;
+        height: 100%;
+        border: 1px solid black;
+        padding: 6px;
+
+        .form-name {
+            display: flex;
+
+            .input-name {
+                margin-right: 15px;
+            }
+        }
+
+        .list-user {
+            .title {
+                font-size: 18px;
+                margin-top: 12px;
+            }
+
+            .user {
+                display: flex;
+
+                .image-user {
+                    margin-right: 20px;
+                }
+            }
+        }
+    }
+
+    .right-block {
+        flex-grow: 1;
+        height: 100%;
+        border: 1px solid black;
     }
 `;
